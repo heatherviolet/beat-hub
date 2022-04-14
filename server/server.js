6b37ffc66aa52a358fb0df116ad1c6cf59202f5f
StaticCloud <diegomarrs@gmail.com>
2022-04-14 14:37:06 -0700

    established connection to mongodb

--- server/server.js
+++ server/server.js
@@ -2,7 +2,19 @@
 const express = require('express');
 const app = express();
 
+// require the code for connecting to the db
+const db = require('./config/connection');
+
 // server port
 const PORT = 3001 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// parse the URL-encoded data using the qs library, allowing for JSON-like experience
+app.use(express.urlencoded({ extended: true }));
+
+// only parse JSON
+app.use(express.json());
+
+// open the database
+db.once('open', () => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
