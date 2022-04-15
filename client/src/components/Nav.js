a81cc6d85754e64ae6cfa229c7eda50bfc91a43a
DrDano <danielharned@gmail.com>
2022-04-14 20:04:59 -0700

    adding nav component with basic CSS styling, moving assets to client, adding bootstrap CDN to index.html

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Navbar, Container, Nav } from "react-bootstrap"
+import logo from "../assets/images/beethub.JPG"
 
-export default function Nav() {
+export default function Navigate() {
   return (
-    <div>Nav</div>
+    <Navbar bg="dark" variant="dark" className="d-flex w-100 justify-content-start">
+    <Container className="d-flex justify-content-start mx-0">
+      <Navbar.Brand>
+        <img
+          alt=""
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+      BeetHub
+      </Navbar.Brand>
+      <Nav className="">
+        <Nav.Link className="">Home</Nav.Link>
+        <Nav.Link className="">Search</Nav.Link>
+        <Nav.Link className="">Profile</Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
   )
 }
