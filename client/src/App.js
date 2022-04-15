bc34c91cf012af21c5547a84a6a85defc8c7409d
DrDano <danielharned@gmail.com>
2022-04-14 21:11:22 -0700

    adding css variables, Nav component routing and styles, and Router to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
-import Nav from "./components/Nav"
-require("bootstrap");
+import "./App.css";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header d-flex align-items-start justify-content-start">
-        <Nav />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header d-flex align-items-start justify-content-start">
+          <Nav />
+        </header>
+        <Routes>
+          <Route exact path="/" />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Navbar, Container, Nav } from "react-bootstrap"
+import { Link } from "react-router-dom";
 import logo from "../assets/images/beethub.JPG"
 
 export default function Navigate() {
+
   return (
-    <Navbar bg="dark" variant="dark" className="d-flex w-100 justify-content-start">
+    <Navbar bg="dark" variant="dark" className="d-flex w-100 justify-content-start" id="navbar">
     <Container className="d-flex justify-content-start mx-0">
-      <Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">
         <img
           alt=""
           src={logo}
@@ -16,10 +18,10 @@ export default function Navigate() {
         />{' '}
       BeetHub
       </Navbar.Brand>
-      <Nav className="">
-        <Nav.Link className="">Home</Nav.Link>
-        <Nav.Link className="">Search</Nav.Link>
-        <Nav.Link className="">Profile</Nav.Link>
+      <Nav>
+        <Nav.Link as={Link} to="/">Home</Nav.Link>
+        <Nav.Link as={Link} to="/search">Search</Nav.Link>
+        <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
       </Nav>
     </Container>
   </Navbar>
