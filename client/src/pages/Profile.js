57f593e955e2767608363ec65989924d4a92ddc6
StaticCloud <diegomarrs@gmail.com>
2022-04-16 17:47:29 -0700

    redirect to login if logged out user tries to view profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
+import Auth from '../utils/auth';
+
+// query user data via apollo client
+import { useQuery } from '@apollo/client';
+
+import { QUERY_ME } from '../utils/queries';
+
 export default function Profile() {
+  const { loading, data } = useQuery(QUERY_ME);
+
+  const user = data?.me
+
+  console.log(user)
+
   return (
-    <div>Profile</div>
+    // conditionally render the user profile
+    <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>Welcome, {user.username}</h1>
+        </div>
+      )}
+    </>
   )
 }
---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+    query {
+        me {
+            _id
+            username
+            email
+            collections {
+                _id
+            }
+            reviews {
+                _id
+            }
+            favorites {
+                _id
+            }
+        }
+    }
+`;
