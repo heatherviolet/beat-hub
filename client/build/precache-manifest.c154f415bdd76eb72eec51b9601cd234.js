1b91de4dc363fda9bf3660a59a6139829520d93a
Heather Violet <heatherviolet91@gmail.com>
2022-04-17 16:58:50 -0700

    commiting changes

---
+++ client/build/precache-manifest.c154f415bdd76eb72eec51b9601cd234.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a59e832047e6bd40db187849c1579067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66c5128920d468a84024",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "cdc82d205f78ca7b31df",
+    "url": "/static/css/main.0a7ca812.chunk.css"
+  },
+  {
+    "revision": "66c5128920d468a84024",
+    "url": "/static/js/2.746a89fd.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.746a89fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc82d205f78ca7b31df",
+    "url": "/static/js/main.81c49e89.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "128365de9f172625c4f891d45c5e36ab",
+    "url": "/static/media/beethub.128365de.JPG"
+  }
+]);
