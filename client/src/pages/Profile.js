285fc72fc8ba9a108e712a96da4a77170a6557d6
StaticCloud <diegomarrs@gmail.com>
2022-04-16 17:47:40 -0700

    redirect to login if logged out user tries to view profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -7,10 +7,18 @@ import { useQuery } from '@apollo/client';
 
 import { QUERY_ME } from '../utils/queries';
 
+import { Redirect } from 'react-router-dom';
+
 export default function Profile() {
   const { loading, data } = useQuery(QUERY_ME);
 
+  // check if the data exists before accessing it
   const user = data?.me
+
+  // redirect to login if the user tries to view the profile and they're not logged in
+  if (!Auth.loggedIn()) {
+    return <Redirect to='/login'/>
+  }
 
   console.log(user)
 
