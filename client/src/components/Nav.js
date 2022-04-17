e4aa46e2ff0cf82005d58f5e0f4f7a61e7cea282
StaticCloud <diegomarrs@gmail.com>
2022-04-16 17:35:41 -0700

    added conditional nav rendering

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,12 +3,20 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/beethub.JPG";
 
+import Auth from '../utils/auth'
+
 export default function Navigate() {
   const [selectedNav, setSelectedNav] = useState("");
+
   const select = (e) => {
     let target = e.target.text;
     setSelectedNav(target);
   };
+
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  }
 
   return (
     <Navbar
@@ -34,15 +42,25 @@ export default function Navigate() {
           <Nav.Link as={Link} to="/search" onClick={select} className={selectedNav === "Search" ? "selectedNav" : ""}>
             Search
           </Nav.Link>
-          <Nav.Link as={Link} to="/profile" onClick={select} className={selectedNav === "Profile" ? "selectedNav" : ""}>
-            Profile
-          </Nav.Link>
-          <Nav.Link as={Link} to="/login" onClick={select} className={selectedNav === "Login" ? "selectedNav" : ""}>
-            Login
-          </Nav.Link>
-          <Nav.Link as={Link} to="/signup" onClick={select} className={selectedNav === "SignUp" ? "selectedNav" : ""}>
-            SignUp
-          </Nav.Link>
+          {Auth.loggedIn() ? (
+            <>
+              <Nav.Link as={Link} to="/profile" onClick={select} className={selectedNav === "Profile" ? "selectedNav" : ""}>
+                Profile
+              </Nav.Link>
+              <Nav.Link to="/" onClick={logout}>
+                Logout
+              </Nav.Link>
+            </>
+          ) : (
+            <>
+              <Nav.Link as={Link} to="/login" onClick={select} className={selectedNav === "Login" ? "selectedNav" : ""}>
+                Login
+              </Nav.Link>
+              <Nav.Link as={Link} to="/signup" onClick={select} className={selectedNav === "Sign Up" ? "selectedNav" : ""}>
+                Sign Up
+              </Nav.Link>
+            </>
+          )}
         </Nav>
       </Container>
     </Navbar>
