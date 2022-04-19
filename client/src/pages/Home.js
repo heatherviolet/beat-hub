7af7879f2303b88e720347d74e6eb8c50f3a8f94
Juan Chavez <juan@Juans-Air.home>
2022-04-18 20:28:34 -0700

    created the home page front end

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,46 @@
 import React from 'react'
+import LatestCollections from '../components/LatestCollections';
+import LatestReviews from '../components/LatestReviews';
+
+import { useQuery } from '@apollo/client';
+import { GET_REVIEWS } from '../utils/queries'
+import { GET_COLLECTIONS } from '../utils/queries'
+
+
 
 export default function Home() {
+
+  const { loadingReviews, reviewsData } = useQuery(GET_REVIEWS);
+  const reviews = reviewsData?.reviews || [];
+  console.log(reviews);
+
+  const { loadingCollections, collectionsData } = useQuery(GET_COLLECTIONS);
+  const collections = collectionsData;
+  console.log(collectionsData);
+  
   return (
-    <div>Home</div>
+    <>
+
+      { loadingCollections ? (
+
+        <LatestCollections collections={collections}></LatestCollections>
+
+      ) : (
+
+        <p>Loading Collections... </p>
+
+      )}
+
+      { loadingReviews ? (
+
+        <LatestReviews reviews={reviews}></LatestReviews>
+
+      ) : (
+
+        <p>Loading Reviews... </p>
+
+      )}
+
+    </>
   )
 }
