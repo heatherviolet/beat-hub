df04f32944d5a916b11e22373d2a146177356ffe
Heather Violet <heatherviolet91@gmail.com>
2022-04-19 22:44:31 -0700

    restyled signup form

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
+
 import Auth from '../utils/auth';
 
 const Signup = () => {
@@ -41,37 +42,70 @@ const Signup = () => {
     <main className="flex-row justify-center mb-4">
       <div className="col-12 col-md-6">
         <div className="card form">
-          <h4 className="card-header">Sign Up</h4>
+          <h4 className="card-header"
+          style={{color: '#3e535f',
+          }}>Sign Up</h4>
           <div className="card-body">
             <form onSubmit={handleFormSubmit}>
+              <label>
+                Enter a username
+              </label>
               <input
                 className="form-input"
-                placeholder="Your username"
+                placeholder="Username"
                 name="username"
                 type="username"
                 id="username"
+                style={{color: '#32434d',
+                                
+                                width: '100%',
+                                backgroundColor: 'white',
+                                padding: '5px',
+                                borderRadius: '5px'
+                            }}
                 value={formState.username}
                 onChange={handleChange}
               />
+              <label>
+                Enter your email
+              </label>
               <input
                 className="form-input"
-                placeholder="Your email"
+                placeholder="Email"
                 name="email"
                 type="email"
                 id="email"
+                style={{color: '#32434d',
+                                
+                                width: '100%',
+                                backgroundColor: 'white',
+                                padding: '5px',
+                                borderRadius: '5px'
+                            }}
                 value={formState.email}
                 onChange={handleChange}
               />
+              <label>
+                Enter a password
+              </label>
               <input
                 className="form-input"
                 placeholder="******"
                 name="password"
                 type="password"
                 id="password"
+                style={{color: '#32434d',
+                                
+                                width: '100%',
+                                backgroundColor: 'white',
+                                padding: '5px',
+                                borderRadius: '5px'
+                            }}
                 value={formState.password}
                 onChange={handleChange}
               />
-              <button className="btn d-block w-100" type="submit">
+              <button id="signupbtn" className="btn d-block w-100" type="submit"
+             >
                 Submit
               </button>
             </form>
