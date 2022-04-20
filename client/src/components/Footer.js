f807a3ad03b13732b3016c939cce1b4d41f0de8b
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-04-19 20:08:14 -0700

    footer and nav logos

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import logo from "../assets/images/beatfoot3.png";
+
 
 export default function Footer(albumData) {
   const year = new Date().getFullYear();
@@ -6,6 +8,13 @@ export default function Footer(albumData) {
   return (
       <div className="footerMain d-flex justify-content-between">
         <div className="footerElement" id="BeetHub">
+        <img 
+            alt=""
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
           BeetHub
         </div>
         <div>{year}</div>
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -29,8 +29,8 @@ export default function Navigate() {
           <img 
             alt=""
             src={logo}
-            width="30"
-            height="30"
+            width="40"
+            height="40"
             className="d-inline-block align-top"
           />{" "}
           BeetHub
